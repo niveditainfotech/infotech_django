895b99e03d653c6934d5bca44dbfdf348bd6d4c0
niveditainfotech <niveditaneetacademy@gmail.com>
2024-03-18 13:35:11 +0530

    new

--- nishainfotech/settings.py
+++ nishainfotech/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-n$ejzqkp+d-he&%@k%*ud1=riqsb61g=-chml22(km#li2u#xs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.11']
 
 
 # Application definition
